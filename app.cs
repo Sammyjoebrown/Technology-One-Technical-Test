84a31506667cb49cca760d8dc076974a5ca1c4d5
Sammyjoebrown <sammyjoebrownme@gmail.com>
2024-11-19 00:03:41 +1000

    Implement main application structure and update welcome message in index.html

--- app.cs
+++ app.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
+namespace TechnologyOneTechTest
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
